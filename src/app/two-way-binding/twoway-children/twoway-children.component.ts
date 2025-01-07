9233a92c42ca47d8f2f9b431f3d8be4137d53d8b
JimKao <jim_kao@misystex.com>
2025-01-07 17:21:12 +0800

    雙向綁定

--- src/app/two-way-binding/twoway-children/twoway-children.component.ts
+++ src/app/two-way-binding/twoway-children/twoway-children.component.ts
@@ -9,6 +9,7 @@ export class TwowayChildrenComponent implements OnInit {
   @Input() name: string = '';
   @Input() name2: string = '';
   @Output() emitName = new EventEmitter<string>();
+  @Output() emitName2 = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit(): void {
@@ -16,5 +17,8 @@ export class TwowayChildrenComponent implements OnInit {
   onInput(val: Event) {
     const inputValue = val.target as HTMLInputElement;
     this.emitName.emit(inputValue.value);
+  }
+  onInput2() {
+    console.log(123)
   }
 }
